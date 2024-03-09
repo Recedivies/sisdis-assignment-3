38eee959f94aad9780904cc574c4f3771a02bb03
recedivies <ahmadhiprananta@gmail.com>
2024-03-09 10:39:46 +0700

    Formatting

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ import node
 
 list_nodes = []
 
-logging.basicConfig(format='%(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-                    datefmt='%Y-%m-%d:%H:%M:%S',
-                    level=logging.DEBUG)
+logging.basicConfig(
+    format="%(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
+    datefmt="%Y-%m-%d:%H:%M:%S",
+    level=logging.DEBUG,
+)
 logger = logging.getLogger(__name__)
+
 
 class NodeProcess(multiprocessing.Process):
 
@@ -26,12 +29,16 @@ class NodeProcess(multiprocessing.Process):
 
 def reload_logging_config_node(filename):
     from importlib import reload
+
     reload(logging)
-    logging.basicConfig(format='%(asctime)-4s %(levelname)-6s %(threadName)s:%(lineno)-3d %(message)s',
-                        datefmt='%H:%M:%S',
-                        filename=f"logs/{filename}",
-                        filemode='w',
-                        level=logging.DEBUG)
+    logging.basicConfig(
+        format="%(asctime)-4s %(levelname)-6s %(threadName)s:%(lineno)-3d %(message)s",
+        datefmt="%H:%M:%S",
+        filename=f"logs/{filename}",
+        filemode="w",
+        level=logging.DEBUG,
+    )
+
 
 def handle_exception(exc_type, exc_value, exc_traceback):
     logger.error(f"Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
@@ -54,13 +61,17 @@ def manual_event_input(args, starting_port, port_used, number_of_nodes):
         elif "r" in input_value:
             logger.info("Restart node input is working...")
             node_id = int(input_value[1:])
-            process = NodeProcess(target=node.main, args=(
-                float(args.heartbeat),
-                float(args.fault_duration),
-                starting_port + node_id - 1,
-                node_id, port_used,
-                True
-            ))
+            process = NodeProcess(
+                target=node.main,
+                args=(
+                    float(args.heartbeat),
+                    float(args.fault_duration),
+                    starting_port + node_id - 1,
+                    node_id,
+                    port_used,
+                    True,
+                ),
+            )
             list_nodes[node_id - 1] = process
             process.start()
             logger.info(f"Node {node_id} has running...")
@@ -75,14 +86,24 @@ def manual_event_input(args, starting_port, port_used, number_of_nodes):
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("-n", type=str, dest="node",
-                        help="The number of nodes", default="4")
-    parser.add_argument("-b", type=str, dest="heartbeat",
-                        help="The particular duration of the heartbeat", default=1)
-    parser.add_argument("-f", type=str, dest="fault_duration",
-                        help="The particular duration to assume a leader node to be a fault", default=1.5)
-    parser.add_argument("-p", type=str, dest="port",
-                        help="Starting port", default=6574)
+    parser.add_argument(
+        "-n", type=str, dest="node", help="The number of nodes", default="4"
+    )
+    parser.add_argument(
+        "-b",
+        type=str,
+        dest="heartbeat",
+        help="The particular duration of the heartbeat",
+        default=1,
+    )
+    parser.add_argument(
+        "-f",
+        type=str,
+        dest="fault_duration",
+        help="The particular duration to assume a leader node to be a fault",
+        default=1.5,
+    )
+    parser.add_argument("-p", type=str, dest="port", help="Starting port", default=6574)
     args = parser.parse_args()
 
     sys.excepthook = handle_exception
@@ -102,22 +123,24 @@ def main():
     for node_id in range(number_of_nodes):
         logger.info(f"Run node {node_id+1}...")
         reload_logging_config_node(f"node{node_id + 1}.txt")
-        process = NodeProcess(target=node.main, args=(
-            float(args.heartbeat),
-            float(args.fault_duration),
-            starting_port + node_id,
-            node_id + 1, port_used
-        ))
+        process = NodeProcess(
+            target=node.main,
+            args=(
+                float(args.heartbeat),
+                float(args.fault_duration),
+                starting_port + node_id,
+                node_id + 1,
+                port_used,
+            ),
+        )
         process.start()
         list_nodes.append(process)
     logger.info("Done running multiple nodes...")
     logger.debug(f"number of running processes: {len(list_nodes)}")
 
-
     logger.info("Execute manual_event_input()...")
     manual_event_input(args, starting_port, port_used, number_of_nodes)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- node.py
+++ node.py
@@ -3,13 +3,23 @@ import threading
 
 
 def thread_exception_handler(args):
-    logging.error(f"Uncaught exception", exc_info=(args.exc_type, args.exc_value, args.exc_traceback))
+    logging.error(
+        f"Uncaught exception",
+        exc_info=(args.exc_type, args.exc_value, args.exc_traceback),
+    )
 
 
 class Raft:
 
-    def __init__(self, node_id: int, port: int, neighbors_ports: list, lb_fault_duration: int, is_continue: bool,
-                 heartbeat_duration: float):
+    def __init__(
+        self,
+        node_id: int,
+        port: int,
+        neighbors_ports: list,
+        lb_fault_duration: int,
+        is_continue: bool,
+        heartbeat_duration: float,
+    ):
         self.heartbeat_duration = heartbeat_duration
         self.is_continue = is_continue
         self.lb_fault_duration = lb_fault_duration
@@ -26,14 +36,23 @@ def reload_logging_windows(filename):
     log = logging.getLogger()
     for handler in log.handlers:
         log.removeHandler(handler)
-    logging.basicConfig(format='%(asctime)-4s %(levelname)-6s %(threadName)s:%(lineno)-3d %(message)s',
-                        datefmt='%H:%M:%S',
-                        filename=filename,
-                        filemode='w',
-                        level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)-4s %(levelname)-6s %(threadName)s:%(lineno)-3d %(message)s",
+        datefmt="%H:%M:%S",
+        filename=filename,
+        filemode="w",
+        level=logging.INFO,
+    )
 
-def main(heartbeat_duration=1, lb_fault_duration=1, port=1000,
-         node_id=1, neighbors_ports=(1000,), is_continue=False):
+
+def main(
+    heartbeat_duration=1,
+    lb_fault_duration=1,
+    port=1000,
+    node_id=1,
+    neighbors_ports=(1000,),
+    is_continue=False,
+):
     reload_logging_windows(f"logs/node{node_id}.txt")
     threading.excepthook = thread_exception_handler
     try:
@@ -46,7 +65,14 @@ def main(heartbeat_duration=1, lb_fault_duration=1, port=1000,
         logging.debug(f"is_continue: {is_continue}")
 
         logging.info("Create raft object...")
-        raft = Raft(node_id, port, neighbors_ports, lb_fault_duration, is_continue, heartbeat_duration)
+        raft = Raft(
+            node_id,
+            port,
+            neighbors_ports,
+            lb_fault_duration,
+            is_continue,
+            heartbeat_duration,
+        )
 
         logging.info("Execute raft.start()...")
         raft.start()
